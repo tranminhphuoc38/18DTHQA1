3d641723f1229c098e5bb6296746c4c324b37905
Minh Phuoc <tranminhphuoc@gmail.com>
2020-09-27 09:52:22 +0700

    Add Generic ex

---
+++ 04-generic-example/04-generic-example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_generic_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
