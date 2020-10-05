404747d91dcfb2865d17822899d4b478c01c8282
Minh Phuoc <tranminhphuoc@gmail.com>
2020-10-06 00:04:06 +0700

    Add async programming example

---
+++ asyn-programming/asyn-programming/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace asyn_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetData1();
+            GetData2();
+            Console.Read();
+        }
+
+        static void GetData1()
+        {
+            Console.WriteLine("Method 1 started");
+            Console.WriteLine(GetResult("Method 1 completed", 2000));
+        }
+
+        static void GetData2()
+        {
+            Console.WriteLine("Method 2 started");
+            Console.WriteLine(GetResult("Method 2 completed", 4000));
+        }
+
+        static string GetResult(string msg, int threadTime)
+        {
+            System.Threading.Thread.Sleep(threadTime);
+            return msg;
+
+        }
+    }
+}
