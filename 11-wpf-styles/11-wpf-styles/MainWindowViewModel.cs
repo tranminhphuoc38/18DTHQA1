14dcdd1b973a38c92e87b5984cee87706f930da0
mpt <mpt@elca.vn>
2020-10-19 23:31:05 +0700

    Update sample code for lesson 11

--- 11-wpf-styles/11-wpf-styles/MainWindowViewModel.cs
+++ 11-wpf-styles/11-wpf-styles/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _11_wpf_styles.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,10 @@ using System.Threading.Tasks;
 
 namespace _11_wpf_styles {
     public class MainWindowViewModel {
+        public List<Class> ListClass { get; set; } = new List<Class> {
+            new Class("Class A1", 2019),
+            new Class("Class A2", 2020),
+            new Class("Class A3", 2021),
+        };
     }
 }
